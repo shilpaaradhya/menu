24ac86164ce5ae28d5c84cecad39581a9857b153
shilpaaradhya <shilpaaradhya411@gmail.com>
2021-10-03 08:09:04 +0530

    initial commit

---
+++ public/dist/express/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+   
+
+});// END $(document).ready();
+
+// $(window).on('load',function(){
+//     $('#launch-modal').modal('show');
+//     $('#dailyrate-modal').modal('show');
+//     //$('#launch-modal').addClass('zIndex-1051');
+// });
